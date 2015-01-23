6045701e276c291d67b64f90aa35a6747b583f02
katcarr <kathrynelizabethcarr@gmail.com>
2015-01-23 11:25:38 -0800

    Add base requirements in app.rband layout for webapp

---
+++ app.rb
@@ -0,0 +1,12 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/client")
+require("./lib/stylist")
+require('pg')
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get('/') do
+  erb(:index)
+end
